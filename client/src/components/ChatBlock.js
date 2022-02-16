849539ac37bf0bc6952e93736c2f8091dda79b95
Asim <asim.awan@outlook.com>
2022-02-15 20:48:26 -0500

    Increase chat window height

--- client/src/components/ChatBlock.js
+++ client/src/components/ChatBlock.js
@@ -11,7 +11,7 @@ function ChatBlock({username, setUsername, setRoom, joinRoom, socket, userEmail,
   const toggleOpen = () => setIsOpen(!isOpen);
 
   const chatStyle = {
-    height: isOpen ? "400px" : "0",
+    height: isOpen ? "650px" : "0",
     width: isOpen ? "350px" : "0",
     opacity: isOpen ? "1" : "0.7",
     boxShadow: isOpen ? "0 0 0px 1px var(--mblue), 0 0 0px 3px rgba(240, 241, 243, 0.8)" : "none"
