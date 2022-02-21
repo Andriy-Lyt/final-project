61827080eb1f6e89fae78408ea9d8a05667f1b81
Andriy-Lyt <howdy_eh@icloud.com>
2022-02-21 18:44:58 -0500

    add cors

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
 const http = require("http");
-// const cors = require("cors");
+const cors = require("cors");
 const {Server} = require("socket.io");
 
-// app.use(cors());
+app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server /* , {
+const io = new Server(server  , {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
-} */);
+});
 
 io.on("connection", (socket) => {
   console.log("user connected", socket.id);
