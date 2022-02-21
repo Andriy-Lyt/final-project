7baa61ccf8700e7e63b46decf41db4c57af0aa41
Andriy-Lyt <howdy_eh@icloud.com>
2022-02-21 15:16:14 -0500

    server pkg json added a line

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {Server} = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server, {
+const io = new Server(server , {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"]
@@ -31,7 +31,9 @@ io.on("connection", (socket) => {
   })
 });
 
-server.listen(3001, () => {
-  console.log("Server is up on port  3001");
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
 
