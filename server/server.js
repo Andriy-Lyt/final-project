66273b119ff58e92822cd76d5c6f31ea13a2c150
Andriy-Lyt <howdy_eh@icloud.com>
2022-02-22 21:03:02 -0500

    correct frontend URL

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server  , {
   cors: {
-    // origin: "http://agent-bubbles.space",
-    origin: "http://localhost:3000",
+    origin: "http://agent-bubbles.space",
+    // origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
 });
